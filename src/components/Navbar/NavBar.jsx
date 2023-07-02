b86ac2d0864f3bcfcbe312d3cca1c439fb0c58be
Cunha <cunhanik1@gmail.com>
2023-07-02 13:27:43 +0100

    fixed

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,9 +5,11 @@ function NavBar() {
   return (
     <div className="h-[10vh] fixed bg-white w-full border-b-2 z-20">
       <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
-      <h1 className="text-[2rem] pl-3 pt-3 font-semibold min-[1920px]:pt-[3vh]">
-        CunhaDev
-      </h1>
+      <a href="#home">
+        <h1 className="text-[2rem] pl-3 pt-3 font-semibold min-[1920px]:pt-[3vh]">
+          CunhaDev
+        </h1>
+      </a>
     </div>
   );
 }
